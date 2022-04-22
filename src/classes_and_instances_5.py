675223aafb01f18609d78b6949460a6adfedde69
denys.horovyi <denys.horovyi@globallogic.com>
2022-04-22 13:00:09 +0200

    Lesson 5
    - classes_and_instances.py_5 has been created
    - added Student class
    - created two objects
    - defined init function within the class with two arguments and additional 'mail' statement
    - defined the fullname and the uppercase functions within the class

---
+++ src/classes_and_instances_5.py
@@ -0,0 +1,33 @@
+class Student:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        self.mail = f'{first}.{last}@gmail.com'
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    def uppercase(self):
+        self.first = self.first.upper()
+        self.last = self.last.upper()
+
+    def email(self):
+        return f'{self.mail}'
+
+
+a1 = Student('Denys', 'Horovyi')
+
+print(a1.fullname())
+print(a1.email())
+
+print(Student.fullname(a1))
+
+print('\n##############################################################\n')
+
+a1.uppercase()
+
+
+print(a1.fullname())
+print(a1.email())
+
+print(Student.fullname(a1))
