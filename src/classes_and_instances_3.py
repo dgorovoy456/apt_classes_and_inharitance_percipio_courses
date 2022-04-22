66bf38f6a5596351a2033cfc99ff230b3308793a
denys.horovyi <denys.horovyi@globallogic.com>
2022-04-22 12:23:42 +0200

    Lesson 2
    - classes_and_instances.py_3 has been created
    - added Student class
    - created three objects
    - defined init function within the class

---
+++ src/classes_and_instances_3.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self):
+        print('Initialize called')
+
+
+b1 = Student()
+b2 = Student()
+b3 = Student()
+
+print('\n########################################################\n')
+
+
+class Student:
+    def __init__(boo):
+        print('Initialize called')
+
+
+a1 = Student()
