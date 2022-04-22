3fbcc6ce2d0dabf033815062a40f7bfee76e32f0
denys.horovyi <denys.horovyi@globallogic.com>
2022-04-22 14:02:14 +0200

    Lesson 6
    - classes_and_instances.py_6 has been created

--- src/classes_and_instances_6.py
+++ src/classes_and_instances_6.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 class Competition:
     raise_amount = 1.04
 
@@ -6,7 +9,8 @@ class Competition:
         self.prize = prize
 
     def raise_prize(self):
-        self.prize = self.prize * Competition.raise_amount
+        # self.prize = self.prize * Competition.raise_amount // or or
+        self.prize = self.prize * self.raise_amount
 
 
 debate = Competition('Debate', 500)
@@ -16,3 +20,7 @@ print(debate.prize)
 debate.raise_prize()
 
 print(debate.prize)
+
+print(debate.__dict__)
+
+pprint(Competition.__dict__)
