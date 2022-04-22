7fdff484c1b571773b398999e2b6271e73d2170e
denys.horovyi <denys.horovyi@globallogic.com>
2022-04-22 11:24:15 +0200

    Lesson 1
    - classes_and_instances.py has been created
    - added Student class
    - created two objects
    - made a few checks of this objects

---
+++ src/classes_and_instances.py
@@ -0,0 +1,18 @@
+class Student:
+    pass
+
+
+print(type(Student))
+
+object_1 = Student()
+
+object_2 = Student()
+
+print(object_1)
+
+print(object_2)
+
+print(isinstance(object_1, Student))
+
+print(isinstance(object_2, Student))
+
